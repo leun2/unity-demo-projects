1c9a0ed4690685b118e1633efe2e2238171b26a5
leun <leun12968@gmail.com>
2025-06-10 01:44:40 +0900

    feat: add Prototype 5 project

---
+++ Prototype 5/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class DifficultyButton : MonoBehaviour
+    {
+        private Button button;
+        private GameManager gameManager;
+        public int difficulty;
+
+        private void Start()
+        {
+            button = GetComponent<Button>();
+            button.onClick.AddListener(SetDifficulty);
+            gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        }
+
+        void SetDifficulty()
+        {
+            gameManager.StartGame(difficulty);
+        }
+    }
+}
---
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class GameManager : MonoBehaviour
+    {
+        private float spawnRate = 1.0f;
+        public List<GameObject> targets;
+        public TextMeshProUGUI scoreText;
+        private int score;
+        public TextMeshProUGUI gameOverText;
+        public bool isGameActive;
+        public Button restartButton;
+        private Coroutine spawnCoroutine;
+        public GameObject titleScreen;
+        
+        public void StartGame(int difficulty)
+        {
+            spawnRate /= difficulty;
+            isGameActive = true;
+            spawnCoroutine = StartCoroutine(SpawnTarget());
+            UpdateScore(0);
+            titleScreen.SetActive(false);
+        }
+
+        IEnumerator SpawnTarget()
+        {
+            while (isGameActive)
+            {
+                yield return new WaitForSeconds(spawnRate);
+                int index = Random.Range(0, targets.Count);
+                Instantiate(targets[index]);
+            }
+        }
+
+        public void UpdateScore(int scoreToAdd)
+        {
+            score += scoreToAdd;
+            scoreText.text = "Score: " + score;
+        }
+        
+        public void GameOver()
+        {
+            gameOverText.gameObject.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+            isGameActive = false;
+            
+            if (spawnCoroutine != null)
+            {
+                StopCoroutine(spawnCoroutine);
+            }
+
+            TargetBase[] allTargets = FindObjectsOfType<TargetBase>();
+            foreach (var target in allTargets)
+            {
+                Destroy(target.gameObject);
+            }
+        }
+
+        public void RestartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        
+
+    }
+}
